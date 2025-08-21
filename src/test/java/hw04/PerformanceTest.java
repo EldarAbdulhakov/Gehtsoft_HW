bdca3f1088a9e878822ca3f96bd263916534cb2f
EldarAbdulhakov <braid2012@gmail.com>
2025-08-21 03:01:20 +0300

    some edits

--- src/test/java/hw04/PerformanceTest.java
+++ src/test/java/hw04/PerformanceTest.java
@@ -3,21 +3,20 @@ package hw04;
 import hw04.CustomHashMap.CustomHashMap;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Set;
 
 public class PerformanceTest {
 
-    private static final int ITERATIONS = 10_000_000;
+    private static final int ITERATIONS = 1_000_000;
     private Integer i;
     private CustomHashMap<Integer, Integer> customHashMap = new CustomHashMap<>();
     private HashMap<Integer, Integer> hashMap = new HashMap<>();
 
-    public void runAndMeasure(Runnable task1, Runnable task2) {
+    public void runAndMeasure(String method,Runnable task1, Runnable task2, Integer iterations) {
         long startTime1 = System.currentTimeMillis();
-        for (i = 0; i < ITERATIONS; i++) {
+        for (i = 0; i < iterations; i++) {
             task1.run();
         }
         task1.run();
@@ -25,42 +24,23 @@ public class PerformanceTest {
         long time1 = endTime1 - startTime1;
 
         long startTime2 = System.currentTimeMillis();
-        for (i = 0; i < ITERATIONS; i++) {
+        for (i = 0; i < iterations; i++) {
             task2.run();
         }
         task2.run();
         long endTime2 = System.currentTimeMillis();
         long time2 = endTime2 - startTime2;
 
-        System.out.printf(" %-15d ms| %-15d ms%n", time1, time2);
+        System.out.printf("%-6s %-8d | %10d ms | %5d ms |%n",method, i, time1, time2);
     }
 
     @Test
-    public void testPutPerformance1() {
-        runAndMeasure(() -> customHashMap.put(i, i), () ->hashMap.put(i, i));
+    public void testPutPerformance() {
+        runAndMeasure("Put", () -> customHashMap.put(i, i), () -> hashMap.put(i, i), ITERATIONS);
     }
 
     @Test
-    public void testPutPerformance2() {
-        long startTime1 = System.currentTimeMillis();
-        for (int i = 0; i < ITERATIONS; i++) {
-            customHashMap.put(i, i);
-        }
-        long endTime1 = System.currentTimeMillis();
-        long time1 = endTime1 - startTime1;
-
-        long startTime2 = System.currentTimeMillis();
-        for (int i = 0; i < ITERATIONS; i++) {
-            hashMap.put(i, i);
-        }
-        long endTime2 = System.currentTimeMillis();
-        long time2 = endTime2 - startTime2;
-
-        System.out.println("customHashMap = " + time1  + " ms, " + "hashMap = " + time2 + " ms");
-    }
-
-    @Test
-    public void testGetPerformance1() {
+    public void testGetPerformance() {
         for (i = 0; i < ITERATIONS; i++) {
             customHashMap.put(i, i);
         }
@@ -68,46 +48,7 @@ public class PerformanceTest {
             hashMap.put(i, i);
         }
 
-        runAndMeasure(() -> customHashMap.get(i), () -> hashMap.get(i));
-    }
-
-    @Test
-    public void testGetPerformance2() {
-        for (i = 0; i < ITERATIONS; i++) {
-            customHashMap.put(i, i);
-        }
-
-        for (i = 0; i < ITERATIONS; i++) {
-            hashMap.put(i, i);
-        }
-
-        long startTime1 = System.currentTimeMillis();
-        for (i = 0; i < ITERATIONS; i++) {
-            customHashMap.get(i);
-        }
-        long endTime1 = System.currentTimeMillis();
-        long time1 = endTime1 - startTime1;
-
-        long startTime2 = System.currentTimeMillis();
-        for (i = 0; i < ITERATIONS; i++) {
-            hashMap.get(i);
-        }
-        long endTime2 = System.currentTimeMillis();
-        long time2 = endTime2 - startTime2;
-
-        System.out.println("customHashMap = " + time1  + " ms, " + "hashMap = " + time2 + " ms");
-    }
-
-    @Test
-    public void testRemovePerformance1() {
-        for (i = 0; i < ITERATIONS; i++) {
-            customHashMap.put(i, i);
-        }
-        for (i = 0; i < ITERATIONS; i++) {
-            hashMap.put(i, i);
-        }
-
-        runAndMeasure(() -> customHashMap.remove(i), () -> hashMap.remove(i));
+        runAndMeasure("Get", () -> customHashMap.get(i), () -> hashMap.get(i), ITERATIONS);
     }
 
     @Test
@@ -115,46 +56,20 @@ public class PerformanceTest {
         for (i = 0; i < ITERATIONS; i++) {
             customHashMap.put(i, i);
         }
-
         for (i = 0; i < ITERATIONS; i++) {
             hashMap.put(i, i);
         }
 
-        long startTime1 = System.currentTimeMillis();
-        for (i = 0; i < ITERATIONS; i++) {
-            customHashMap.remove(i);
-        }
-        long endTime1 = System.currentTimeMillis();
-        long time1 = endTime1 - startTime1;
-
-        long startTime2 = System.currentTimeMillis();
-        for (i = 0; i < ITERATIONS; i++) {
-            hashMap.remove(i);
-        }
-        long endTime2 = System.currentTimeMillis();
-        long time2 = endTime2 - startTime2;
-
-        System.out.println("customHashMap = " + time1  + " ms, " + "hashMap = " + time2 + " ms");
+        runAndMeasure("Remove", () -> customHashMap.remove(i), () -> hashMap.remove(i), ITERATIONS);
     }
 
-//    @ParameterizedTest
-//    public void testMapsPerformance() {
-//
-//    }
-
-
-//    @Test
-//    public void testMapsPerformance() {
-//        System.out.println("Maps Performance Test\n");
-//        System.out.printf("%-12s %-10s %-16s %-18s%n", "Array size |", "Shift    |", "ArrayCopy (ms) |", "Manual Loop (ms) |");
-//        System.out.println("---------------------------------------------------------");
-//
-//                System.out.printf("%-11d| %-9d|", size, shift);
-//                runAndMeasure(() -> ArrayOperations.shiftLeftSystemCopy(arraySystemCopy, shift),
-//                        () -> ArrayOperations.shiftLeftManualLoop(arrayManualLoop, shift));
-//
-//                System.out.println("---------------------------------------------------------");
-//
-//
-//    }
+    @ParameterizedTest()
+    @ValueSource(ints = {10_000, 100_000, 1_000_000, 10_000_000})
+    public void testMapsPerformance(Integer iterations) {
+        System.out.println("--------------------------------------------");
+        System.out.printf("%-17s %-16s %-18s%n", "Iterations      |", "CustomHashMap |", "HashMap |");
+        runAndMeasure("Put", () -> customHashMap.put(i, i), () -> hashMap.put(i, i), iterations);
+        runAndMeasure("Get", () -> customHashMap.get(i), () -> hashMap.get(i), iterations);
+        runAndMeasure("Remove", () -> customHashMap.remove(i), () -> hashMap.remove(i), iterations);
+    }
 }
